91af74c991d483f12fee444a4118a18e071055e9
cena <mk.cena92@gmail.com>
2012-11-15 01:39:41 +0200

    analysis ass1 update 3

--- PiggyBank.java
+++ PiggyBank.java
@@ -1,43 +1,40 @@
-
 public class PiggyBank {
-
-	/**
-	 * @param args
-	 */
-	static int dp[][];
-	public static int DP(int [] money, int m,int q, int i)
+	static String dp[][];
+	public static String DP(int [] money, int m,int q, int i)
 	{
 		if(q>m || q<0)
-			return -1;
+			return "-1,;";
 		
 		if(i==money.length)
-			return q;
+			return q+",;";
 		else
-			if(dp[q][i]!=-2)
+			if(dp[q][i]!=null)
 				return dp[q][i];
 			else
 			{
-				int max=Math.max(DP(money, m, q+money[i], i+1), DP(money, m, q-money[i], i+1));
+				String[]split1=DP(money, m, q+money[i], i+1).split(",");
+				String[]split2=DP(money, m, q-money[i], i+1).split(",");
+				int a=Integer.parseInt(split1[0]);
+				int b=Integer.parseInt(split2[0]);
+				String max=(a>b)?(a==-1?a+",,":a+","+"+"+split1[1]):(b==-1?b+",,":b+","+"-"+split2[1]);
 				dp[q][i]=max;
 				return max;
 			}
 	}
-	
-	public static int getMaxAmount(int [] money,int m, int q)
+	public static String getMaxAmount(int [] money,int m, int q)
 	{
-		dp=new int[1002][52];
-		for(int i=0;i<dp.length;i++)
-			for(int j=0;j<dp[i].length;j++)
-				dp[i][j]=-2;
-		return DP(money,m,q,0);
+		dp=new String[1002][52];
+		String max=DP(money,m,q,0);
+		return max.substring(0, max.length()-1);
 	}
 	
 	public static void main(String[] args) {
 		int money [] = {74,39,127,95,63,140,99,96,154,18,137,162,14,88};
 		int q=40;
 		int m=243;
+		
+		
 		System.out.println(getMaxAmount(money,m,q));
 
 	}
-
 }
